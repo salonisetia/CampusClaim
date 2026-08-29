a443ad07df803982adc1413f3a24f876de7c2257
Saloni Setia <salonisetia2005@gmail.com>
2026-08-29 21:03:10 +0530

    Configure backend for Vercel

--- backend/index.js
+++ backend/index.js
@@ -52,3 +52,4 @@ app.patch('/api/verify_item/:id', async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
+module.exports = app;
